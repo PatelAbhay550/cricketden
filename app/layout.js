1329d90cbbcaef9f91b25648dc2ac7c5680de435
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-12-16 23:47:17 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
+    <meta name="google-site-verification" content="7-6j7haeyJWEhikD2wtLIv8Ei8tdX7VOIO3amkGNYdo" />
     <meta name="google-site-verification" content="CP-bBZfPoKzhXDdIAAeb5AvIDbZoMMJToPGRiBTbb00" />
     </head>
       <body className={inter.className}>
