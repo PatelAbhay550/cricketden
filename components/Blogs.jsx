9cc22f317f984292926a0739fb0977c63cd0222f
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-12-18 11:44:02 +0530

    Update Blogs.jsx

--- components/Blogs.jsx
+++ components/Blogs.jsx
@@ -41,7 +41,7 @@ const Blogs = async () => {
                 src={blog.coverimage}
                 alt={blog.title}
                
-                className="rounded-t-lg aspect-square object-cover"
+                className="rounded-t-lg  object-cover"
               />
             </div>
             <div className="p-4">
