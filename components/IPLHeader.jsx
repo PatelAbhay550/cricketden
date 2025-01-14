57a6a8427f0e026cf559490455af931de4dd7b6b
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2025-01-14 16:35:14 +0530

    Update IPLHeader.jsx

--- components/IPLHeader.jsx
+++ components/IPLHeader.jsx
@@ -3,12 +3,9 @@ import { FaArrowLeft } from "react-icons/fa";
 
 const IPLHeader = () => {
   return (
-    <header className="flex items-center gap-8 py-4 px-4 text-white bg-gradient-to-r to-blue-500 via-blue-600 from-blue-700">
-      <div className="back">
-        <Link href="/">
-          <FaArrowLeft size={32} />
-        </Link>
-      </div>
+    <header className="flex items-center gap-8 py-4 px-6 text-white bg-gradient-to-r to-blue-500 via-blue-600 from-blue-700">
+     
+       
       <div className="title-container">
         <h1 className="title text-xl font-bold">Indian Premier League</h1>
         <h2>2024 Season</h2>
