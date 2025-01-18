fd8a5b1e9a483fd2bb9f78410aebd87bbb26701d
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2025-01-18 22:54:08 +0530

    Update Container.jsx

--- components/Container.jsx
+++ components/Container.jsx
@@ -1,10 +1,30 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import { TbLiveView } from "react-icons/tb";
+import { TbLiveView, TbBrandBlogger } from "react-icons/tb";
 import Link from "next/link";
+import { db } from "@/firebase";
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import Image from "next/image";
 
+// Fetch blogs server-side
+export async function getBlogs() {
+  const blogsRef = collection(db, "allblogs");
+  const q = query(blogsRef);
+  const querySnapshot = await getDocs(q);
+
+  return querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+}
 const HomePage = async () => {
+  const blogs = await getBlogs();
+
+  // Helper function to truncate text
+  const truncate = (text, length) => {
+    if (text.length > length) {
+      return `${text.slice(0, length)}...`;
+    }
+    return text;
+  };
   // Get current date and date 5 days later in the required format (YYYYMMDD)
   const currentDate = new Date();
   const futureDate = new Date();
@@ -147,6 +167,40 @@ const HomePage = async () => {
             </p>
           )}
         </div>
+        <h2 className="text-2xl flex items-center gap-2 font-bold text-primary mt-8 mb-4">
+          <TbBrandBlogger className="text-rose-600 text-3xl" />
+          Blogs to Read
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+          {blogs.map((blog) => (
+            <div
+              key={blog.id}
+              className="blog-card bg-white shadow-md rounded-lg overflow-hidden "
+            >
+              <Link href={`/Blogs/${blog.slug}`}>
+                <div className="relative w-full h-48 overflow-hidden">
+                  <img
+                    src={blog.coverimage}
+                    alt={blog.title}
+                    className="rounded-t-lg w-full h-full object-cover"
+                  />
+                </div>
+                <div className="p-4">
+                  <p className="text-sm text-gray-500">{blog.category}</p>
+                  <h3 className="text-lg font-bold mt-2">{blog.title}</h3>
+                  <p className="text-gray-600 text-sm mt-1">
+                    {truncate(blog.desc || "No description available.", 100)}
+                  </p>
+                  <p className="text-gray-500 text-xs mt-2">
+                    {new Date(
+                      blog.timestamp.seconds * 1000
+                    ).toLocaleDateString()}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
         <h2 className="text-2xl font-bold text-primary mt-8 mb-4">
           Upcoming Matches
         </h2>
@@ -251,7 +305,7 @@ const HomePage = async () => {
                   </div>
                   <div className="text-center mt-2">
                     <p className="text-gray-600 text-xs">
-                       Match Status: {match.match_result}
+                      Match Status: {match.match_result}
                     </p>
                   </div>
                 </div>
