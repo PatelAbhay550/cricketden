bafb32469e49278111e102a4a02278d2e2ce6c9a
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2025-05-14 16:41:58 +0530

    Update page.jsx

--- app/ipl2025/stats/page.jsx
+++ app/ipl2025/stats/page.jsx
@@ -7,7 +7,7 @@ export const metadata = {
   keywords:
     "IPL 2025, IPL 2025 stats, IPL 2025 Standings, IPL 2025 Teams, IPL 2025 squads",
   openGraph: {
-    title: "IPL 2025 All Stats| Runs, Wickets, Fours, Sixes - Cricketden,
+    title: "IPL 2025 All Stats| Runs, Wickets, Fours, Sixes - Cricketden",
     description:
       "IPL 2025 All Stats, A dashboard that displays the top run scorers, top wicket takers, most fours, and most sixes from players of the IPL 2025 season from  MI, CSK, RCB, RR, PBKS, KKR, DC, SRH, GT, and LSG ",
     url: "https://cricketden.vercel.app/IPL-2025/stats",
