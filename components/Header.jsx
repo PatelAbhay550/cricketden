2a9b8ed656af907986e8f7c66f9ad7d75b8fc245
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2025-05-15 09:35:26 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <nav className="navbar flex items-center justify-between px-4 h-16  text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-800">
         <div className="le">
-          <Link href='/' className="flex items-center">
+          <Link href='/' className="flex items-center ink">
             <p className="text-2xl font-bold" title="Cricketden" aria-label="site-logo">Cricket<span className="text-[#E63946]">den</span></p>
             </Link>
         </div>
