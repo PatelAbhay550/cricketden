c8f86106bedc3199cb6a38b808cc0d033857b43d
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2025-05-15 09:39:26 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import MenuHome from "@/components/MenuHome";
 import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
     </head>
       <body className={inter.className}>
         <Header />
+    <MenuHome />
         {children}<Analytics/>
         <Footer />
       </body>
