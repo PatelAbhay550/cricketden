07c898fea165987db8d24befc1e5d1921988913c
Abhay Patel <patelabhay550@gmail.com>
2026-01-13 11:16:45 +0530

    nuked sitemap.xml

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sz5c/aXd9nnz6xuX/OalcMOW7N2f/wcPBRjFrplqfdo=\"\n}"
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "53d75b54e2354ed6"; }
+/******/ 		__webpack_require__.h = function() { return "23353c2dccb159b7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
