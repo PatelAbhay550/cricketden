8c030e643bd7a22faf88a5685388e7966c960f27
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2026-01-21 12:53:23 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import MenuHome from "@/components/MenuHome";
-import { Analytics } from "@vercel/analytics/react"
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,18 +19,16 @@ export default function RootLayout({ children }) {
     <html lang="en">
     <head>
     <meta name="google-site-verification" content="7-6j7haeyJWEhikD2wtLIv8Ei8tdX7VOIO3amkGNYdo" />
-    <meta name="google-site-verification" content="CP-bBZfPoKzhXDdIAAeb5AvIDbZoMMJToPGRiBTbb00" />
+    
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8804622562841633" crossorigin="anonymous"></script>
     </head>
       <body className={inter.className}>
         <Header />
     <MenuHome />
-        {children}<Analytics/>
-    <a href="/" alt="page hit counter" target="_blank" >
-<embed src="//s10.histats.com/109.swf"  flashvars="jver=1&acsid=4958585&domi=4"  quality="high"  width="150" height="20" name="109.swf"  align="middle" type="application/x-shockwave-flash" pluginspage="//www.macromedia.com/go/getflashplayer" wmode="transparent" /></a>
-<img  src="//sstatic1.histats.com/0.gif?4958585&101" alt="site stats" border="0"/>
+        {children}
         <Footer />
       </body>
     </html>
   );
 }
+
